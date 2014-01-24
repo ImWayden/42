6061e2bf72c6f1348757d902b1fd2cac54408d0e
Mouhamadou SARR <msarr@e1r13p14.42.fr>
2014-01-24 21:16:20 +0100

    tout

---
+++ ft_select/ft_select/ft_setlist.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_addlist.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msarr <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/12/22 14:41:15 by msarr             #+#    #+#             */
+/*   Updated: 2013/12/22 14:41:19 by msarr            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+static int		ft_listlen(t_list *list)
+{
+	t_list		*tmp;
+	int			i;
+
+	i = 0;
+	tmp = list;
+	if (tmp)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	while (tmp != list)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
+
+t_list		*ft_setlist(t_list *list)
+{
+	int		j;
+	int		k;
+	struct winsize mywin;
+	int				i;
+	t_list			*tmp;
+
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &mywin);
+	i = ft_listlen(list);
+	j = 1;
+	if (i > mywin.ws_row)
+		while ((j * mywin.ws_row) < i && mywin.ws_row)
+			j++;
+	k = 0;
+	i = 0;
+	tmp = list;
+	tmp->row = k;
+	tmp->col = k;
+	i++;
+	tmp = tmp->next;
+	while (tmp != list)
+	{
+		while (i < mywin.ws_row && tmp != list)
+		{
+			tmp->row = i;
+			tmp->col = k * mywin.ws_col / j;
+			tmp = tmp->next;
+			i++;
+		}
+		i = 0;
+		k++;
+	}
+	return (list);
+}
