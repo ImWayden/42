2997d08f3bb97e4c2b664e5355b117f39c3ee67e
msarr <admin@admins-mac.home>
2014-10-22 12:29:30 +0200

    all

---
+++ WEB/42project/app/cache/dev/twig/c7/7e/c509c65726d44690c18df0579b65fa10d846a66f9130c9a2237f2c67ff02.php
@@ -0,0 +1,49 @@
+<?php
+
+/* AcmeProjectBundle::layout.html.twig */
+class __TwigTemplate_c77ec509c65726d44690c18df0579b65fa10d846a66f9130c9a2237f2c67ff02 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'sidebar' => array($this, 'block_sidebar'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 4
+    public function block_sidebar($context, array $blocks = array())
+    {
+        // line 5
+        echo "    Sidebar content
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeProjectBundle::layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 5,  28 => 4,);
+    }
+}
---
+++ WEB/42project/src/Acme/ProjectBundle/AcmeProjectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeProjectBundle extends Bundle
+{
+}
