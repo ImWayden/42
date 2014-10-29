6574f3ae6c9be757d80cab9a2fbc1df13f8a0767
msarr <admin@admins-mac.home>
2014-10-29 10:58:31 +0100

    minishell2

--- C/Unix1/minishell2/srcs/builtins/cd.c
+++ C/Unix1/minishell2/srcs/builtins/cd.c
@@ -25,36 +25,77 @@ static int			s_setenv(t_env **envs, char *name, char **arg)
 		{
 			while (envc && envc->next && ft_strcmp(envc->name, name))
 				envc = envc->next;
-			if (!ft_strcmp(envc->name, name))
+			if (envc && !ft_strcmp(envc->name, name))
+			{
+				ft_memdel((void **)&envc->arg);
 				envc->arg = ft_strdup(args[0]);
+			}
 			return (EXIT_SUCCESS);
 		}
 	}
 	return (EXIT_FAILURE);
 }
 
+static void			cd_get_path(char **av, char **path, char *pwd, char *str)
+{
+	char			*str1;
+
+	if (!av[1])
+		*path = ft_strdup(str);
+	else if (!ft_strncmp(av[1], "~", 1))
+		*path = ft_strjoin(str, &(av[1][1]));
+	else if (!ft_strncmp(av[1], "/", 1))
+		*path = ft_strdup(av[1]);
+	else if (!ft_strcmp(av[1], "-"))
+		*path = NULL;
+	else if (!ft_strcmp(av[1], ".") || !ft_strcmp(av[1], ".."))
+	{
+		*path = ft_strdup(pwd);
+		if (!ft_strcmp(av[1], ".."))
+			(ft_strrchr(*path, '/'))[1] = '\0';
+	}
+	else
+	{
+		str1 = ft_strjoin(pwd, "/");
+		*path = ft_strjoin(str1, av[1]);
+		ft_memdel((void **)&str1);
+	}
+}
+
+static void			ft_del(char **path, char **pwd, char **home)
+{
+	ft_memdel((void **)path);
+	ft_memdel((void **)pwd);
+	ft_memdel((void **)home);
+}
+
 int					cd(t_shell *shell, t_tree *tree)
 {
-	t_env			*envs;
-	char			**av;
 	char			*pwd;
+	char			*path;
+	char			*home;
+	int				i;
 
-	envs = shell->env;
-	av = tree->argv;
-	pwd = get_env(envs, "PWD");
-	if (av[1] == NULL || !ft_strcmp(av[1], "~") || !ft_strcmp(av[1], "~/"))
-		av[1] = get_env(envs, "HOME");
-	else if (!ft_strcmp(av[1], "-"))
-		av[1] = get_env(envs, "OLDPWD");
-	if (chdir(av[1]) == -1)
+	pwd = get_env(shell->env, "PWD");
+	home = get_env(shell->env, "HOME");
+	cd_get_path(tree->argv, &path, pwd, home);
+	if (!path)
 	{
-		ft_putmsg(av[1], ": cd : No such file or directory.");
+		path = get_env(shell->env, "OLDPWD");
+		ft_putendl(path);
+	}
+	if (chdir(path) == -1)
+	{
+		ft_putmsg(tree->argv[1], ":cd : No such file or directory.");
 		return (EXIT_FAILURE);
 	}
 	else
 	{
-		s_setenv(&envs, "OLDPWD", &pwd);
-		s_setenv(&envs, "PWD", &av[1]);
+		while ((i = ft_strlen(path)) && path[i - 1] == '/')
+			path[i - 1] = '\0';
+		s_setenv(&(shell->env), "OLDPWD", &pwd);
+		s_setenv(&(shell->env), "PWD", &path);
 	}
+	ft_del(&path, &pwd, &home);
 	return (EXIT_SUCCESS);
 }
